ea75189e4abbe20d527a16ea203614eb3987dba3
glerpstudios <samuelsapatla@gmail.com>
2023-12-06 22:18:47 -0500

    v
    f

--- quiz.js
+++ quiz.js
@@ -1,32 +1,445 @@
 // Questions that will be asked
 const Questions = [{
-    q: "Question 1",
-    a: [{ text: "Incorrect", isCorrect: false },
-    { text: "Incorrect", isCorrect: false },
-    { text: "Correct", isCorrect: true },
-    { text: "Incorrect", isCorrect: false }
+    q: "Question 1?", thing: 1,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
     ]
  
 },
 {
-    q: "Question 2?",
-    a: [{ text: "Incorrect", isCorrect: false, isSelected: false },
-    { text: "Incorrect", isCorrect: false },
-    { text: "Incorrect", isCorrect: false },
-    { text: "Correct", isCorrect: true }
+    q: "Question 20?", thing: 20,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
     ]
  
 },
 {
-    q: "Question 3",
-    a: [{ text: "Incorrect", isCorrect: false },
-    { text: "Incorrect", isCorrect: false },
-    { text: "Correct", isCorrect: true },
-    { text: "Incorrect", isCorrect: false }
+    q: "Question 21", thing: 21,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 300?", thing: 300,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 301?", thing: 301,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 310", thing: 310,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 311?", thing: 311,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 4000?", thing: 4000,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 4001", thing: 4001,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 4010?", thing: 4010,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 4011?", thing: 4011,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 4100", thing: 4100,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 4101?", thing: 4101,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 4110?", thing: 4110,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 4111", thing: 4111,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 50000?", thing: 50000,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 50001?", thing: 50001,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 50010", thing: 50010,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 50011?", thing: 50011,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 50100?", thing: 50100,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 50101", thing: 50101,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 50110?", thing: 50110,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 50111?", thing: 50111,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 51000", thing: 51000,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 51001?", thing: 51001,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 51010?", thing: 51010,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 51011", thing: 51011,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 51100?", thing: 51100,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 51101?", thing: 51101,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 51110", thing: 51110,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 51111?", thing: 51111,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 600000?", thing: 600000,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 600001", thing: 600001,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 600010?", thing: 600010,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 600011?", thing: 600011,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 600100", thing: 600100,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 600101?", thing: 600101,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 600110?", thing: 600110,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 600111", thing: 600111,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 601000?", thing: 601000,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 601001?", thing: 601001,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 601010", thing: 601010,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 601011?", thing: 601011,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 601100?", thing: 601100,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 601101", thing: 601101,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 601110?", thing: 601110,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 601111?", thing: 601111,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 610000", thing: 610000,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 610001?", thing: 610001,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 610010?", thing: 1,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 610011", thing: 1,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 610100?", thing: 1,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 610101?", thing: 1,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 610110", thing: 1,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 610111?", thing: 1,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 611000?", thing: 1,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 611001", thing: 1,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 611010?", thing: 1,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 611011?", thing: 1,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 611100", thing: 1,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 611101?", thing: 1,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 611110?", thing: 1,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
+    ]
+ 
+},
+{
+    q: "Question 611111", thing: 1,
+    a: [{ text: "Incorrect", isCorrect: false,  ans: a },
+    { text: "Incorrect", isCorrect: false, ans: b }
     ]
  
 }
- 
 ]
  
 let currQuestion = 0
