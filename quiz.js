1eab377ddaa217e57b36caf90d0b2066dbc448e3
glerpstudios <samuelsapatla@gmail.com>
2023-12-09 12:34:02 -0500

    tehdthr

--- quiz.js
+++ quiz.js
@@ -3,143 +3,163 @@ let startingval = 10;
 const Conversion = [0, "a", "b", "c", "d", "e", "f", "g", "h", "i", 1, "a", "b", "c", "d", "e", "f", "g", "h", "i", 3, 2, "b", "c", "d", "e", "f", "g", "h", "i", 6, 5, 4, "c", "d", "e", "f", "g", "h", "i", 10, 9, 8, 7, "d", "e", "f", "g", "h", "i", 15, 14, 13, 12, 11, "e", "f", "g", "h", "i", 21, 20, 19, 18, 17, 16];
 const Questions = [{
     q: "Question 10?",
-    a: [{ text: "Incorrect", isCorrect: false},
-    { text: "Correct", isCorrect: true}
-    ]
- 
+    a: [{ text: "You run a trustworthy antivirus scan to remove any potential malware from your device.", isCorrect: true},
+    { text: "You call 1-877-438-4338 to report that you are a victim of identity theft.", isCorrect: false}
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 21?",
-    a: [{ text: "Incorrect", isCorrect: false},
-    { text: "Correct", isCorrect: true}
-    ]
- 
+    a: [{ text: "File a data breach case; immediately restore data from an offline backup.", isCorrect: false},
+    { text: "Immediately remove the malware from your device using trustworthy antivirus, then Owen must inform his contacts that his device has been compromised and emails from Owen’s computer are malicious.", isCorrect: true}
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 20?",
-    a: [{ text: "Incorrect", isCorrect: false},
-    { text: "Correct", isCorrect: true}
-    ]
- 
+    a: [{ text: "File a data breach case; immediately restore data from an offline backup.", isCorrect: false},
+    { text: "Immediately remove the malware from your device using trustworthy antivirus, then Owen must inform his contacts that his device has been compromised and emails from Owen’s computer are malicious.", isCorrect: true}
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 32?",
-    a: [{ text: "Incorrect", isCorrect: false},
-    { text: "Correct", isCorrect: true}
-    ]
- 
+    a: [{ text: "Attempt to remove Owen’s email address from the black web, this will remove the source of the issue.", isCorrect: false},
+    { text: "Install a firewall to filter phishing emails out.", isCorrect: true}
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 31?",
-    a: [{ text: "Incorrect", isCorrect: false},
-    { text: "Correct", isCorrect: true}
-    ]
- 
+    a: [{ text: "File a data breach. Inform the customers. Follow the disaster recovery plan.", isCorrect: true},
+    { text: "Run antivirus software on all the organization’s computers to effectively resolve the problems.", isCorrect: false}
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 30?",
-    a: [{ text: "Incorrect", isCorrect: false},
-    { text: "Correct", isCorrect: true}
-    ]
- 
+    a: [{ text: "Have all the passwords of his employees changed and have the employees’ contacts be warned that the employees’ device has been compromised, and have them run antivirus software.", isCorrect: true},
+    { text: "Owen can simply replace the infected devices with new uncompromised devices.", isCorrect: false}
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 43?",
-    a: [{ text: "Incorrect", isCorrect: false},
-    { text: "Correct", isCorrect: true}
-    ]
- 
+    a: [{ text: "The email appears suspicious because the link is suspicious.", isCorrect: true},
+    { text: "The email appears suspicious because the link is suspicious.", isCorrect: false}
+    ],
+    showImage: true, // Add a property to indicate whether to show image
+    imagePath: "osha-violation.jpeg" // Add the path to the image 
 },
 {
     q: "Question 42?",
     a: [{ text: "Incorrect", isCorrect: false},
     { text: "Correct", isCorrect: true}
-    ]
- 
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 41?",
-    a: [{ text: "Incorrect", isCorrect: false},
-    { text: "Correct", isCorrect: true}
-    ]
- 
+    a: [{ text: "Incorrect", isCorrect: true},
+    { text: "Correct", isCorrect: false}
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 40?",
     a: [{ text: "Incorrect", isCorrect: false},
     { text: "Correct", isCorrect: true}
-    ]
- 
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 54?",
-    a: [{ text: "Incorrect", isCorrect: false},
-    { text: "Correct", isCorrect: true}
-    ]
- 
+    a: [{ text: "Incorrect", isCorrect: true},
+    { text: "Correct", isCorrect: false}
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 53?",
     a: [{ text: "Incorrect", isCorrect: false},
     { text: "Correct", isCorrect: true}
-    ]
- 
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 52?",
-    a: [{ text: "Incorrect", isCorrect: false},
-    { text: "Correct", isCorrect: true}
-    ]
- 
+    a: [{ text: "Incorrect", isCorrect: true},
+    { text: "Correct", isCorrect: false}
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 51?",
     a: [{ text: "Incorrect", isCorrect: false},
     { text: "Correct", isCorrect: true}
-    ]
- 
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 50?",
-    a: [{ text: "Incorrect", isCorrect: false},
-    { text: "Correct", isCorrect: true}
-    ]
- 
+    a: [{ text: "Incorrect", isCorrect: true},
+    { text: "Correct", isCorrect: false}
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 65?",
     a: [{ text: "Incorrect", isCorrect: false},
     { text: "Correct", isCorrect: true}
-    ]
- 
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 64",
     a: [{ text: "Incorrect", isCorrect: false},
     { text: "Correct", isCorrect: true}
-    ]
- 
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 63?",
     a: [{ text: "Incorrect", isCorrect: false},
     { text: "Correct", isCorrect: true}
-    ]
- 
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 62?",
     a: [{ text: "Incorrect", isCorrect: false},
     { text: "Correct", isCorrect: true}
-    ]
- 
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 61",
     a: [{ text: "Incorrect", isCorrect: false},
     { text: "Correct", isCorrect: true}
-    ]
- 
+    ],
+    showImage: false, // Add a property to indicate whether to show image
+    imagePath: "image1.jpg" // Add the path to the image 
 },
 {
     q: "Question 60?",
@@ -152,29 +172,44 @@ const Questions = [{
 
 let score = 0
  
+
 function loadQues() {
-    const question = document.getElementById("ques")
-    const opt = document.getElementById("opt")
- 
-    question.textContent = Questions[Conversion[startingval] - 1].q;
-    opt.innerHTML = ""
- 
-    for (let i = 0; i < 6; i++) {
-        const choicesdiv = document.createElement("div");
-        const choice = document.createElement("input");
-        const choiceLabel = document.createElement("label");
-   
-        choice.type = "radio";
-        choice.name = "answer";
-        choice.value = i;
-   
-        choiceLabel.textContent = Questions[Conversion[startingval] - 1].a[i].text;
-   
-        choicesdiv.appendChild(choice);
-        choicesdiv.appendChild(choiceLabel);
-        opt.appendChild(choicesdiv);
+    const questionContainer = document.getElementById("ques");
+    const opt = document.getElementById("opt");
+    const imageContainer = document.getElementById("image-container"); // New container for image
+  
+    const currentQuestion = Questions[Conversion[startingval] - 1];
+    questionContainer.textContent = currentQuestion.q;
+    opt.innerHTML = "";
+  
+    if (currentQuestion.showImage) {
+      // Display the image if showImage is true
+      const image = document.createElement("img");
+      image.src = currentQuestion.imagePath;
+      imageContainer.innerHTML = "";
+      imageContainer.appendChild(image);
+    } else {
+      // Clear the image container if showImage is false
+      imageContainer.innerHTML = "";
     }
-}
+  
+    for (let i = 0; i < 6; i++) {
+      const choicesdiv = document.createElement("div");
+      const choice = document.createElement("input");
+      const choiceLabel = document.createElement("label");
+  
+      choice.type = "radio";
+      choice.name = "answer";
+      choice.value = i;
+  
+      choiceLabel.textContent = currentQuestion.a[i].text;
+  
+      choicesdiv.appendChild(choice);
+      choicesdiv.appendChild(choiceLabel);
+      opt.appendChild(choicesdiv);
+    }
+  }
+  
  
 loadQues();
  
@@ -209,11 +244,25 @@ function loadScore() {
 }
  
  
+
+function loadFeedbackMessage(isCorrect) {
+    const feedbackContainer = document.getElementById("feedback");
+    const feedbackMessage = document.createElement("p");
+
+    feedbackMessage.textContent = isCorrect ? "Correct! Well done." : "Incorrect. Keep trying!";
+    feedbackContainer.innerHTML = "";
+    feedbackContainer.appendChild(feedbackMessage);
+}
+
 function nextQuestion() {
     const selectedAns = parseInt(document.querySelector('input[name="answer"]:checked').value);
 
     // Determine the next question based on the boolean value of isCorrect
-    const nextQuestionIndex = Questions[Conversion[startingval] - 1].a[selectedAns].isCorrect
+    const isCorrect = Questions[Conversion[startingval] - 1].a[selectedAns].isCorrect;
+
+    loadFeedbackMessage(isCorrect);
+
+    const nextQuestionIndex = isCorrect
         ? startingval + 11  // Go to the next question if the answer is correct
         : startingval + 10; // Skip the next question if the answer is incorrect
 
@@ -224,18 +273,17 @@ function nextQuestion() {
         document.getElementById("opt").remove();
         document.getElementById("ques").remove();
         document.getElementById("btn").remove();
+        document.getElementById("feedback").remove();
         loadScore();
     }
 }
- 
+
 function checkAns() {
     const selectedAns = parseInt(document.querySelector('input[name="answer"]:checked').value);
- 
+
     if (Questions[Conversion[startingval] - 1].a[selectedAns].isCorrect) {
         score++;
-        console.log("Correct")
-        nextQuestion();
-    } else {
-        nextQuestion();
     }
+
+    nextQuestion();
 }
